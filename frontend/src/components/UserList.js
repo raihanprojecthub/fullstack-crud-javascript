f0b07580995f0128b5f1a9d9aeae7a71c1f4f2e1
raihanprojecthub <raihanramadhanp09@gmail.com>
2024-11-10 21:51:37 +0700

    u-101124

---
+++ frontend/src/components/UserList.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const UserList = () => {
+  return (
+    <div className="columns mt-5 is-centered">
+      <div className="column is-half">
+        <table className="table is-staped is-fullwidth">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Gender</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default UserList
