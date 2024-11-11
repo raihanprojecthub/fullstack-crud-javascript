9b04fd81ec9f0a3eb5452975394e0f90565acadf
raihanprojecthub <raihanramadhanp09@gmail.com>
2024-11-11 21:54:05 +0700

    u-111124

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
 const UserList = () => {
+const [users, setUser] = useState([]);
+
+useEffect(()=>{
+  getUsers();
+},[])
+
+ const getUsers = async () =>{
+  const response = await axios.get('http://localhost:5000/users');
+  setUser(response.data)
+ }
+
   return (
     <div className="columns mt-5 is-centered">
       <div className="column is-half">
@@ -15,13 +27,18 @@ const UserList = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
+            {users.map((user, index) => (
+              <tr key={user.id}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.gender}</td>
+                <td>
+                  <button className='button is-small is-info'>Edit</button>
+                  <button className='button is-small is-danger'>Delete</button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
